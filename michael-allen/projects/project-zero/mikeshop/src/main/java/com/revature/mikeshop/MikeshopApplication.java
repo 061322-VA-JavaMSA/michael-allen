244aff2075b89266ef121c791a2523176cf93dd6
mallen92 <107433030+mallen92@users.noreply.github.com>
2022-06-22 15:47:31 -0700

    Add files via upload

---
+++ michael-allen/projects/project-zero/mikeshop/src/main/java/com/revature/mikeshop/MikeshopApplication.java
@@ -0,0 +1,13 @@
+package com.revature.mikeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikeshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikeshopApplication.class, args);
+	}
+
+}
